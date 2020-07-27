26a3d67e20e79055cf10915bef2a9238d0afdda6
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-27 18:30:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,5 +4,7 @@ end
 introduction("josh")
 
 def introduction_with_language(name, language)
-puts  
+puts "Hi, my name is #{name} and I am learning to program in #{language}." 
 end
+
+introduction_with_language("Josh", "")
